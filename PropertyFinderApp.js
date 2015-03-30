61d0c79e6f4771be1551fe1d8b1e8b7825c80d47
Corey Speisman <Cspeisman@gmail.com>
2015-03-29 23:04:47 -0400

    first commit

---
+++ PropertyFinderApp.js
@@ -0,0 +1,40 @@
+// your code goes here
+'use strict';
+
+var React = require('react-native'),
+    SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class HelloWorld extends React.Component {
+  render() {
+    return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+  }
+}
+
+
+var PropertyFinderApp = React.createClass({
+  render: function(){
+    return (
+      <React.NavigatorIOS 
+        style={styles.container}
+        initialRoute={{
+          title: "Property Finder",
+          component: SearchPage
+        }} />
+    )
+  }
+})
+
+
+React.AppRegistry.registerComponent('PropertyFinderApp', function() { return PropertyFinderApp });
